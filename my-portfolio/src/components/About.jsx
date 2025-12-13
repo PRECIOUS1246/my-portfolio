ffe7e5a470c1b6cbaa11a9bf8fda61617013027c
PRECIOUS1246 <oseipreciousmagdalene@gmail.com>
2025-12-13 23:41:42 +0000

    first commit

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -1,13 +1,48 @@
+import { FaCode, FaBriefcase, FaGraduationCap, FaLaptopCode } from "react-icons/fa";
+
 function About() {
   return (
     <section id="about" className="about">
       <h2>About Me</h2>
-      <p>I build modern, fast, responsive React applications.I am a frontend developer passionate about building clean, responsive,
-        and interactive web applications. I enjoy working with React, Tailwind
-        CSS, and other modern web technologies to create projects that are both
-        functional and visually appealing.</p>
+
+      <p className="about-text">
+        I build modern, fast, responsive React applications. I am a frontend
+        developer passionate about building clean, responsive, and interactive
+        web applications. I enjoy working with React, Tailwind CSS, and other
+        modern web technologies to create projects that are both functional and
+        visually appealing.
+      </p>
+
+      <div className="info-cards">
+        <div className="info-card">
+          <FaCode className="info-icon" />
+          <h4>Skill</h4>
+          <p>Frontend developer & proficient in Microsoft Office</p>
+        </div>
+
+        <div className="info-card">
+          <FaBriefcase className="info-icon" />
+          <h4>Experience</h4>
+          <p>4 Years</p>
+        </div>
+
+        <div className="info-card">
+          <FaGraduationCap className="info-icon" />
+          <h4>Education</h4>
+          <p>
+            OLA Senior High School <br />
+            Ghana Communication Technology University
+          </p>
+        </div>
+
+        <div className="info-card">
+          <FaLaptopCode className="info-icon" />
+          <h4>Languages</h4>
+          <p>Java, C++, HTML, CSS, JavaScript, React</p>
+        </div>
+      </div>
     </section>
   );
 }
 
-export default About;
+export default About;
--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -3,8 +3,8 @@ import pic from "../assets/pic.jpg";
 function Hero() {
   return (
     <div id="home" className="hero">
-      <h1>Hello, I'm Magtech</h1>
-      <p>Frontend Developer | React Specialist</p>
+      <h1>Hello, I'm Magdalene Precious Osei</h1>
+      <p>Frontend Developer | React Specialist|System Analyst</p>
       <img className="picture" src={pic} alt="Profile picture" />
 
       
